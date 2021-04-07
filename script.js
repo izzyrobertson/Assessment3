9ddac88b7b6941cad114c48542c0d4e495ab8a04
izzyrobertson <79881525+izzyrobertson@users.noreply.github.com>
2021-04-08 11:46:01 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log('javascript');
+
+$(document).ready(function(){
+
+  $("#home, #book, #locationspage, #contact").hide();
+
+  $("#homeNav").click(function(){
+    $("#home").show();
+    $("#services, #book, #locationspage, #contact").hide();
+  });
+
+  $("#servicesNav").click(function(){
+    $("#services").show();
+    $("#home, #book, #locationspage, #contact").hide();
+  });
+
+  $("#bookNav").click(function(){
+    $("#book").show();
+    $("#home, #services, #locationspage, #contact").hide();
+  });
+
+  $("#locationspageNav").click(function(){
+    $("#locationspage").show();
+    $("#home, #services, #book, #contact").hide();
+  });
+
+  $("#contactNav").click(function(){
+    $("#contact").show();
+    $("#home, #services, #book, #locationspage").hide();
+  });
+});
+
+  // $("#mainMenu").hide();
+  //
+  // $("#main").click(function(){
+  //   $("#mainMenu").show();
+  //   $("#startersMenu, #drinksMenu, #dessertMenu").hide();
+  // });
